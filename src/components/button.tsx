1904a7de27a0bab622741a45275467f8a8e2885b
Goran <gviljanac@gmail.com>
2023-03-30 15:39:08 +0200

    naknadno sto sam se zajebavo

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const Button = () => {
+  const [modal,setModal] =useState(false);
+  const toggle = () => {
+    setModal(!modal);
+  }
   return (
     <div className="btn">
-      <button className="btn__click">Text</button>
+      <button onClick={toggle} className="btn__click">Text</button>
+      {modal ? (<>
+      <p onClick={toggle}>maaaaaaaaaaaaaaaaaaaaaaaaaaaaasala</p>
+      </>):''}
     </div>
   );
 };
