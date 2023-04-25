3bf4a7eaebaf1c98097d554df9ce6c55ffdab4c9
Goran <gviljanac@gmail.com>
2023-04-25 11:37:53 +0200

    for petlje zadaca 25.04 from home

--- src/pages/petlj.tsx
+++ src/pages/petlj.tsx
@@ -29,11 +29,77 @@ const Petlje = () => {
     // let opseg = Math.PI * inputValue * 2;
     // console.log("radius kruga je: ", opseg, "cm");
 
-    let ispis = "";
-    for (let i = 0; i <= 10; i++) {
-      ispis += i + ", ";
+    //4. Ispiši brojeve koji su string u niz
+
+    // let ispis = "";
+    // for (let i = 0; i <= 10; i++) {
+    //   ispis += i + ", ";
+    // }
+    // console.log(ispis);
+
+    //Domaća zadaća
+    //1.Print numbers from 0 to 15
+    
+    // for(let i =0;i <=15; i++){
+    //   console.log(i)
+    // }
+
+    //2.Print numbers from 12 to 24
+    
+    // for(let i =12;i <=24; i++){
+    //   console.log(i)
+    // }
+
+    //3.Print the ODD numbers from 7 to 31
+    
+    // for(let i =7;i <=31; i++){
+    //   if(i % 2 ){
+    //     console.log(i)
+    //   }
+    // }
+
+    //4.Print the EVEN numbers from 10 to -20
+    
+    // for(let i =10;i >=-20; i--){
+    //   if(i % 2 ==0 ){
+    //     console.log(i)
+    //   }
+    // }
+
+    //5. Iterate through all numbers from 1 to 45. Print the following:
+
+          // For multiples of 3 print “Fizz”
+          // For multiples of 5 print “Buzz”
+          // For multiples of 3 and 5 print “FizzBuzz”
+    
+    // for(let i =1;i <=45; i++){
+    //   if(i % 3 && i%5 ){
+    //     console.log('FizzBuzz  je za višekratnike od 3 i 5')
+    //   } else if(i % 5 ){
+    //     console.log('Buzz      je za višekratnike od 5')
+    //   }else if(i % 3){
+    //     console.log('Fizz      je za višekratnike od 3')
+    //   }   
+    // }
+    
+    //6. Print all the elements of the following array
+      let array =['element1','element2','element3','element4']
+
+    for(let i =0; i < array.length; i++){
+      console.log(array[i]) //zasto je ' i ' wrappan u zagrade []
     }
-    console.log(ispis);
+
+    //7. Calculate the sum of all the numbers in the following array
+
+    // let arrayNum = [1, 12, 1, 1, 1];
+    // let sum = 0;
+    // for (let  i = 0; i <arrayNum.length; i++) {
+    //    sum += arrayNum[i]; 
+    //     }
+    //   console.log("Ukupan rezultat je " + sum );
+
+      //Kraj
+      
   };
   return (
     <div className="container">
