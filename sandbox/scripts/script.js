4a1943192dc54db05eef762074f24c08fa7e7173
grafika10 <grafika10@ucione.local>
2023-05-05 21:57:14 +0200

    klase u javi 5.5 from school

--- sandbox/scripts/script.js
+++ sandbox/scripts/script.js
@@ -1,67 +1,134 @@
-let box = document.getElementsByClassName('box')[0]
+let box = document.getElementsByClassName("box")[0];
 ////1. dohvatimo box i promjenimo mu background-color (kroz JS)
-box.style.backgroundColor ="red";
+box.style.backgroundColor = "red";
 
 //2. dodamo dodatnu klasu na box koja će povecati njegove dimenzije
-
+// box.classList.add('nesto');
 //3.  animiramo box do 30px i nazad (kroz JS)
 let position = 0;
 let reverse = false;
-const frame = () =>{
-    if (reverse) {
-      position--;
+const frame = () => {
+  box.style.top = position + "rem";
+  box.style.right = position + "rem";
+  box.style.position = "absolute";
+
+  if (reverse) {
+    position--;
+  } else {
+    position++;
+  }
+
+  if (position == 30) {
+    reverse = true;
+  }
+  if (position == 0) {
+    reverse = false;
+  }
+};
+const animate = setInterval(frame, 50);
+
+//4. kreiramo funkciju koja će imati lokalnu varijablu u kojoj ćemo izracunati sumu 2 parametra i rezultat ispisati unuatr box-a
+const sum = (a, b) => {
+  let suma = a + b;
+  return suma;
+};
+document.getElementById("suma").innerHTML = sum(12, 12);
+
+//5.dodaj on click i smanji dimenzije boxa
+const handleClick = () => {
+  const currentScale = parseInt(box.style.scale);
+  const newScale = currentScale === 2 ? 1 : 2;
+  box.style.scale = newScale;
+  box.classList.remove("boxing");
+};
+
+//dalje vezbanja
+let box2 = document.getElementsByClassName("box")[1];
+box2.innerHTML = "Ja sam promjenjen čovek   aka box2";
+function hover() {
+  box.classList.add("boxing");
+
+  const currentHeight = parseInt(box2.style.height);
+  const newHeight = currentHeight === 20 ? 10 : 20;
+  box2.style.height = newHeight + "rem";
+}
+
+let stanje;
+let box3 = document.getElementsByClassName("box")[2];
+box3.innerHTML = "Ja sam box3";
+function box3event() {
+  if (stanje === 0) {
+    box3.style = "opacity:1";
+    return (stanje = 1);
+  } else {
+    box3.style = "opacity:0";
+    return (stanje = 0);
+  }
+}
+
+let test = "testovi i bake i zabe";
+document.getElementsByClassName("box")[3].innerHTML = test;
+
+//zadatci jos njih -----------------------------------------------------------------
+
+//Create a class Dog
+function Dog() {
+  this.hasSat = false;
+  this.bark = function () {
+    return console.log("vau vau");
+  };
+  this.doSit = function () {
+    this.hasSat = true;
+  };
+  this.standUp = function () {
+    this.hasSat = false;
+  };
+}
+//Create an instance of Dog class
+const dog = new Dog();
+console.log(dog.hasSat);
+dog.bark();
+dog.doSit();
+console.log(dog.hasSat);
+dog.bark();
+
+console.log(
+  "---------------------------------- Room Klase --------------------------"
+);
+//Zadatak kreiraj klasu Room koja će imati vrijednosti
+//širina, visina i dužina i metode za racunanje površine
+//i prostora sobe. Dodati još vrijednost jeli upaljeno
+//svijetlo i metodu koja će nam u konzolu ispisati trenutno
+//stanje svijelta
+
+function Room() {
+  this.width = 7;
+  this.height = 4;
+  this.length = 6;
+
+  this.povrsina = this.width * this.length;
+  this.volumen = this.width * this.length * this.height;
+
+  this.light = function () {
+    let turned = true;
+    if (turned) {
+      return console.log("Svijetlo je uplaljeno");
     } else {
-      position++;
+      return console.log("Svijetlo je ugašeno");
     }
-
-    let currentPosition = position + "%";
-    if (position == 90) {
-      reverse = true;
-    }
-    if (position == 0) {
-      reverse = false;
-    }
-
-    box.style.marginLeft = currentPosition;
   };
-  const animate = setInterval(frame, 50);
+  let box4 = document.querySelector(".box")[4];
 
- //4. kreiramo funkciju koja će imati lokalnu varijablu u kojoj ćemo izracunati sumu 2 parametra i rezultat ispisati unuatr box-a
- const sum = (a, b ) => {
-    let suma = a + b;
-    return suma;
-  };
-  document.getElementById("suma").innerHTML = sum(12,12);
-  //5.dodaj on click i smanji dimenzije boxa
-  const handleClick = () => {
-    const currentScale = parseInt (box.style.scale);
-    const newScale= currentScale === 2 ? 1 : 2;
-    box.style.scale = newScale 
-    box.classList.remove("boxing");
-  };
-
-  //dalje vezbanja
-  let box2 = document.getElementsByClassName('box')[1]
-  box2.innerHTML= 'Ja sam promjenjen čovek   aka box2'
-  function hover(){
-box.classList.add("boxing");
-
-    const currentHeight = parseInt(box2.style.height);
-    const newHeight = currentHeight === 20 ? 10:20;
-    box2.style.height = newHeight + 'rem';
-  }
-
-  
-  let stanje;
-  let box3 =document.getElementsByClassName('box')[2]
-  box3.innerHTML='Ja sam box3'
-  function box3event(){
-    if(stanje ===0){
-       box3.style='opacity:1';
-      return (stanje =1);
-    }else{
-      box3.style='opacity:0';
-      return (stanje =0);
+  this.switch = function () {
+    let svetlo = false;
+    if (svetlo) {
+      box4.style.backgroundColor = "black";
+    } else {
+      box4.style.backgroundColor = "yellow";
     }
-  }
-  
+  };
+}
+const room = new Room();
+room.light();
+console.log(room.povrsina);
+console.log(room.volumen);
