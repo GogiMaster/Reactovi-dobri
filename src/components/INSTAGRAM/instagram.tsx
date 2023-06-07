b718c7eb3601b8f875c39e437398f9a106be03cb
grafika10 <grafika10@ucione.local>
2023-06-07 21:43:54 +0200

    laganini

--- src/components/INSTAGRAM/instagram.tsx
+++ src/components/INSTAGRAM/instagram.tsx
@@ -1,24 +1,17 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { PostType } from "./types";
-import { v4 as uuidv4 } from 'uuid';
-import Post from "./Posts/post";
-//slike
-import plus from '../../assets/plus.svg'
+import { v4 as uuidv4 } from "uuid";
+import plus from "../../assets/plus.svg";
+import airplane from "../../assets/airplane.svg";
+import profile from "../../assets/profile.svg";
 import trash from "../../assets/trash.svg";
-import profile from '../../assets/profile.svg'
-import heartRed from '../../assets/heart-red.svg'
-import heart from '../../assets/heart.svg'
-import chat from '../../assets/bubble.svg'
-import airplane from '../../assets/airplane.svg'
+import chat from "../../assets/bubble.svg";
 import Like from "./Posts/like";
-
-
-
-const newPostObj:PostType = {
-  id: '2',
+const newPost = {
+  id: 2,
   title: "oblak",
   description: "Ovo je obak sliko sam ga i baš je dope",
-  hashtags: ["oblak", "priroda", "de lajkaj"],
+  hashtag: ["oblak", "priroda", "de lajkaj"],
   img_url:
     "https://techcrunch.com/wp-content/uploads/2022/01/GettyImages-1308797233.jpg",
 };
@@ -26,127 +19,161 @@ const newPostObj:PostType = {
 const Instagram = () => {
   const [modal, setModal] = useState<boolean>(false);
   const [posts, setPosts] = useState<PostType[]>([]);
-//Funkcija za otvaranje modala
-const handleModal=()=>{
-  setModal(!modal)
-}
+  //Funkcija za otvaranje modala
+  const handleModal = () => {
+    setModal(!modal);
+  };
 
-////Funkcija za dohvacanje i ispisivanje post-ova
-const getPosts = () => {
-  fetch("http://localhost:3000/instagram")
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then((post) => {
-      console.log(post);
-      setPosts(post);
-    })
-    .catch((err) => err(err));
-};
-//funkcija za sljanje novih postova na server
-const createPost = (obj: PostType) => {
-  fetch("http://localhost:3000/instagram",
-  {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(obj)})
-      .then((res)=>{
-        if(res.ok){
-          return res.json()
+  ////Funkcija za dohvacanje i ispisivanje post-ova
+  const getPosts = () => {
+    fetch("http://localhost:3000/instagram")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
         }
       })
-      .then((post)=>{
-        
+      .then((post) => {
+        console.log(post);
+        setPosts(post);
+      })
+      .catch((err) => err(err));
+  };
+  //funkcija za sljanje novih postova na server
+  const createPost = (obj: PostType) => {
+    fetch("http://localhost:3000/instagram", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((post) => {
         getPosts();
       })
       .catch((error) => console.log(error));
-};
+  };
 
+  //Za upisivanje vrijednosti u novi post
+  const handlePost = () => {
+    const titleValue = document.querySelector(
+      "#post_title"
+    ) as HTMLInputElement;
+    const descValue = document.querySelector(
+      "#post_desc"
+    ) as HTMLTextAreaElement;
+    const imgValue = document.querySelector("#post_img") as HTMLInputElement;
 
-//Za upisivanje vrijednosti u novi post  
-const handlePost=()=>{
-  const titleValue = document.querySelector(
-    "#post_title"
-  ) as HTMLInputElement;
-  const descValue = document.querySelector(
-    "#post_desc"
-  ) as HTMLTextAreaElement;
-  const imgValue = document.querySelector("#post_img") as HTMLInputElement;
-
-  const newPost:PostType={
-    id:uuidv4(),
-    title:titleValue.value,
-    description:descValue.value ,
-    img_url:`https://source.unsplash.com/random/300×300/?${imgValue.value}`,
-    hashtags:['super','mnogo dobro']
-  }
-  createPost(newPost)
-  setModal(false)
-}
-//za brisanje
-const deletePost=(id:string)=>{
-  fetch("http://localhost:3000/instagram/"+ id,{
-    method:'DELETE',
-  })
-  .then(res =>res.json())
-  .then((res)=>{
-      getPosts()
-  })
-}
-useEffect(() => {
-  getPosts()
-}, []);
+    const newPost: PostType = {
+      id: uuidv4(),
+      title: titleValue.value,
+      description: descValue.value,
+      img_url: `https://source.unsplash.com/random/300×300/?${imgValue.value}`,
+      hashtags: ["super", "mnogo dobro"],
+    };
+    createPost(newPost);
+    setModal(false);
+  };
+  //za brisanje
+  const deletePost = (id: string) => {
+    fetch("http://localhost:3000/instagram/" + id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        getPosts();
+      });
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <>
       <div className="container--sm">
-      <div className="post__tab">
-    <h1>Instagram</h1> 
-    <img className="post__bigger" src={plus} onClick={()=> handleModal()}/>
-    </div>
-    <hr />
-    {posts.map((post)=>{
-      return<>
-       <div className="post" key={post.id}>
-   <div className="post__header">
-    <img  className="post__bigger" src={profile}/>
-   </div>
-   <div className="post__tab">
-    <h1>{post.title}</h1>
-    <img className="post__svg" onClick={()=>deletePost(post.id)} src={trash}/>
+        <div className="post__tab">
+          <h1>Instagram</h1>
+          <img
+            className="post__bigger"
+            src={plus}
+            onClick={() => handleModal()}
+          />
+        </div>
+        <hr />
+        {posts.map((post) => {
+          return (
+            <>
+              <div className="post" key={post.id}>
+                <div className="post__header">
+                  <img className="post__bigger" src={profile} />
+                </div>
+                <div className="post__tab">
+                  <h1>{post.title}</h1>
+                  <img
+                    className="post__svg"
+                    onClick={() => deletePost(post.id)}
+                    src={trash}
+                  />
+                </div>
+                <img className="post__img" src={post.img_url} />
+                <div className="post__bar">
+                  <Like data={post} />
+                  <img className="post__svg" src={chat} />
+                  <img className="post__svg" src={airplane} />
+                </div>
+                <p>{post.description}</p>
+                <span>{"#" + post.hashtags} </span>
+              </div>
+            </>
+          );
+        })}
 
-
-   </div>
-    <img className="post__img" src={post.img_url}  />
-    <div className="post__bar">
-      <Like
-      data={post}
-      />
-      <img className="post__svg" src={chat} />
-      <img className="post__svg" src={airplane} />
-    </div>
-    <p>{post.description}</p>
-    <span>{'#'+ post.hashtags} </span>
-  </div>
-  
- 
-  </>
-})}
-
-{modal ? 
-<>
-<div onClick={handleModal} className="overlay"></div>
-<div className="post__modal">
-  <input id="post_title" required className="post__modal__input" type="text" placeholder="Title" />
-  <textarea id="post_desc" required name="description" className="post__modal__input" placeholder="Description"></textarea>
-  <input id="post_img" required className="post__modal__input" type="text" placeholder="Random Image" />
-  <input id="post_hash" required className="post__modal__input" type="text" placeholder="Hashtags" />
-  <button type="submit" onClick={()=>handlePost()} className="btn">Create Post</button>
-</div>
-</>
-:<></>}
+        {modal ? (
+          <>
+            <div onClick={handleModal} className="overlay"></div>
+            <div className="post__modal">
+              <input
+                id="post_title"
+                required
+                className="post__modal__input"
+                type="text"
+                placeholder="Title"
+              />
+              <textarea
+                id="post_desc"
+                required
+                name="description"
+                className="post__modal__input"
+                placeholder="Description"
+              ></textarea>
+              <input
+                id="post_img"
+                required
+                className="post__modal__input"
+                type="text"
+                placeholder="Random Image"
+              />
+              <input
+                id="post_hash"
+                required
+                className="post__modal__input"
+                type="text"
+                placeholder="Hashtags"
+              />
+              <button
+                type="submit"
+                onClick={() => handlePost()}
+                className="btn"
+              >
+                Create Post
+              </button>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
--- src/pages/timer.tsx
+++ src/pages/timer.tsx
@@ -1,91 +1,87 @@
-import { useEffect, useState } from "react"
-import fireworks from '../assets/Video/fireworks.mp4'
+import { useEffect, useState } from "react";
+import fireworks from "../assets/Video/fireworks.mp4";
 
 const Timer = () => {
-    const [partyTime,setPartyTime]= useState(false)
-    const [days,setDays]= useState(0)
-    const [hours,setHours]= useState(0)
-    const [minutes,setMinutes]= useState(0)
-    const [seconds,setSeconds]= useState(0)
+  const [partyTime, setPartyTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
-useEffect(() => {
- const target = new Date('07/12/2023 17:30:00')
-//  const target = new Date('05/23/2023 13:45:00')
+  useEffect(() => {
+    const target = new Date("07/10/2023 21:55:00");
+    //  const target = new Date('05/23/2023 13:45:00')
 
- const interval = setInterval(()=>{
-    const now= new Date()
-    const diffrence = target.getTime() - now.getTime()
+    const interval = setInterval(() => {
+      const now = new Date();
+      const diffrence = target.getTime() - now.getTime();
 
-    const d = Math.floor(diffrence / (1000 * 60* 60* 24))
-    setDays(d)
+      const d = Math.floor(diffrence / (1000 * 60 * 60 * 24));
+      setDays(d);
 
-    const h = Math.floor(
-        (diffrence % (1000 *60*60*24)) / (1000*60*60)
-    )
-    setHours(h)
+      const h = Math.floor(
+        (diffrence % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
 
-    const m = Math.floor(
-        (diffrence %(1000*60*60))/ (1000*60)
-    )
-    setMinutes(m)
+      const m = Math.floor((diffrence % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
 
-    const s = Math.floor((diffrence % (1000*60 )) / 1000)
+      const s = Math.floor((diffrence % (1000 * 60)) / 1000);
 
-    setSeconds(s)
+      setSeconds(s);
 
-    if(d <= 0 && h <=0 && m <= 0 && s <= 0 ){
-        setPartyTime(true)
-    }
- }, 1000)
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+      }
+    }, 1000);
 
- return () => clearInterval(interval)
-}, [])
-
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    
-<>
+    <>
+      {partyTime ? (
+        <div className="gotovo">
+          <h1 className="gotovo__text">GOTOVOOOOOO</h1>
+          <img
+            className="firework"
+            src="https://reactiongifs.me/cdn-cgi/imagedelivery/S36QsAbHn6yI9seDZ7V8aA/f25f4cf6-87a4-49a3-9625-b68e32c26700/w=500"
+            alt=""
+          />
+        </div>
+      ) : (
+        <div className="container">
+          <h1 className="timer__title">Do kraja tečaja</h1>
 
-
-        {partyTime ? ( 
-           <div className="gotovo">
-            <h1 className="gotovo__text">GOTOVOOOOOO</h1>
-           <img className="firework" src="https://reactiongifs.me/cdn-cgi/imagedelivery/S36QsAbHn6yI9seDZ7V8aA/f25f4cf6-87a4-49a3-9625-b68e32c26700/w=500" alt="" />
-           </div>
-         ):(
-             
-             <div className="container">
-                <h1 className="timer__title">Do kraja tečaja</h1>
-
-        <div className="timer">
-                <div className="border">
-            <div className="timer__section">
+          <div className="timer">
+            <div className="border">
+              <div className="timer__section">
                 <span className="timer__counter">{days}</span>
                 <h1 className="timer__label">Days</h1>
-            </div>
-            <span className="timer__divider">:</span>
-            
-            <div className="timer__section">
+              </div>
+              <span className="timer__divider">:</span>
+
+              <div className="timer__section">
                 <span className="timer__counter">{hours}</span>
                 <h1 className="timer__label">Hours</h1>
-            </div>
-            <span className="timer__divider">:</span>
-            <div className="timer__section">
+              </div>
+              <span className="timer__divider">:</span>
+              <div className="timer__section">
                 <span className="timer__counter">{minutes}</span>
                 <h1 className="timer__label">Minutes</h1>
-            </div>
-            <span className="timer__divider">:</span>
-            <div className="timer__section">
+              </div>
+              <span className="timer__divider">:</span>
+              <div className="timer__section">
                 <span className="timer__counter">{seconds}</span>
                 <h1 className="timer__label">Seconds</h1>
+              </div>
             </div>
+          </div>
         </div>
-        </div>
-    </div>
-         )}
-</>
+      )}
+    </>
+  );
+};
 
-  )
-}
-
-export default Timer
+export default Timer;
